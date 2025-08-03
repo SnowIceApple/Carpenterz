1c38f4eec086da9c49ec68c00cc87aa3b937c84a
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2025-08-04 03:49:04 +0900

    1차 업로드

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+    const swiper1 = new Swiper('.main_slide', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+
+    pagination: {
+        el: '.main-slide-pagination',
+        clickable: true,
+    },
+    });
 });
