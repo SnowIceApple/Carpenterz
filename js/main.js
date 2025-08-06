8ce1fdead1c94f4aaf7fb1f31704c0dccfb39871
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2025-08-07 04:02:29 +0900

    중도 업로드

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+    $('.nav_floor1 > li').on('mouseenter focusin', function(){
+        $(this).children('.nav_floor2').stop().slideDown(300);
+    });
+
+    $('.nav_floor1 > li').on('mouseleave', function(){
+        $(this).children('.nav_floor2').stop().slideUp(300);
+    });
+
     const swiper1 = new Swiper('.main_slide', {
     direction: 'horizontal',
     loop: true,
